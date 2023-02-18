7c1a349b26268f72e9bbd900406cac52adb8fdc5
woongminKi <dndals1991@gmail.com>
2023-02-18 17:01:17 +0900

    chore: edit cors origin

--- modules/socket.js
+++ modules/socket.js
@@ -4,7 +4,8 @@ const Room = require("../models/Room");
 module.exports = (server) => {
   const io = socketIO(server, {
     cors: {
-      origin: process.env.CLIENT_URL,
+      // origin: process.env.CLIENT_URL,
+      origin: "*",
     },
   });
 
